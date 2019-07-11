2c645f5e1d52081cc779df668766172e053e8925
Eita Honjo <nkc.eita.honjo@gmail.com>
2019-07-11 13:34:22 +0900

    基盤を作成中

---
+++ TeamTaniguchi2/Assets/Scripts/BoxGenerate.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxGenerate : MonoBehaviour
+{
+    public GameObject[] box = new GameObject[5];
+    int[] tableBoxTest = { 1, 1, 1, 1, 1, 1, 1, 1 };
+    int[] tableBox01 = { 1, 0, 2, 0, 0, 1, 2, 0 };
+    int[] tableBox02 = { 0, 1, 1, 0, 2, 0, 0, 1 };
+    int[] tableBox03 = { 1, 0, 1, 2, 0, 0, 1, 1 };
+    int[] tableBox04 = { 2, 0, 0, 1, 2, 0, 2, 0 };
+    int[] tableBox05 = { 2, 0, 2, 0, 0, 1, 0, 1 };
+    int[] tableBox06 = { 2, 0, 1, 0, 1, 0, 2, 0 };
+    int[] tableBox07 = { 2, 0, 2, 0, 0, 2, 0, 0 };
+    int[] tableBox08 = { 0, 0, 2, 0, 3, 0, 0, 1 };
+    int[] tableBox09 = { 0, 1, 0, 3, 0, 0, 2, 0 };
+    int[] tableBox10 = { 3, 0, 0, 0, 2, 0, 2, 0 };
+    int[] tableBox11 = { 0, 2, 0, 3, 0, 0, 0, 1 };
+    int[] tableBox12 = { 1, 0, 1, 0, 4, 0, 0, 0 };
+    int[] tableBox13 = { 4, 0, 0, 0, 0, 1, 0, 1 };
+    int[] tableBox14 = { 0, 2, 0, 4, 0, 0, 0, 0 };
+    int[] tableBox15 = { 5, 0, 0, 0, 0, 0, 0, 1 };
+    int[] tbn;
+    float[] scale_x = { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f };//大きさ
+    const float scale_y = 1.0f;
+    const float scale_z = 1.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        for(int i = 0; i < 5; i++)//ボックスの型(1～5の大きさ)
+        {
+            box[i] = (GameObject)Resources.Load("Box" + (i + 1));
+            box[i].transform.localScale = new Vector3(scale_x[i], scale_y, scale_z);
+        }
+        GenerateStart(0);//最初
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	}
+    
+    void GenerateStart(int now)//生成開始
+    {    
+        int rnd = Random.Range(1,16);//呼び出すテーブル
+        float position_x = 0.0f;
+        float position_y = 0.0f;
+        Debug.Log(rnd);
+
+        switch (rnd)
+        {
+            case 1:
+                tbn = tableBox01;
+                break;
+            case 2:
+                tbn = tableBox02;
+                break;
+            case 3:
+                tbn = tableBox03;
+                break;
+            case 4:
+                tbn = tableBox04;
+                break;
+            case 5:
+                tbn = tableBox05;
+                break;
+            case 6:
+                tbn = tableBox06;
+                break;
+            case 7:
+                tbn = tableBox07;
+                break;
+            case 8:
+                tbn = tableBox08;
+                break;
+            case 9:
+                tbn = tableBox09;
+                break;
+            case 10:
+                tbn = tableBox10;
+                break;
+            case 11:
+                tbn = tableBox11;
+                break;
+            case 12:
+                tbn = tableBox12;
+                break;
+            case 13:
+                tbn = tableBox13;
+                break;
+            case 14:
+                tbn = tableBox14;
+                break;
+            case 15:
+                tbn = tableBox15;
+                break;
+        }
+        //tbn = tableBoxTest;
+
+        for (int i = 0; i < 8; i++)//マイナス１になったらテーブル終了
+        {
+
+            switch (tbn[i])
+            {
+                case 0://ゼロは空白だから何もしない
+                    break;
+                case 1:
+                    Instantiate(box[0], new Vector3(position_x, position_y, 0), Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(box[1], new Vector3(position_x, position_y, 0), Quaternion.identity);
+                    break;
+                case 3:
+                    Instantiate(box[2], new Vector3(position_x, position_y, 0), Quaternion.identity);
+                    break;
+                case 4:
+                    Instantiate(box[3], new Vector3(position_x, position_y, 0), Quaternion.identity);
+                    break;
+                case 5:
+                    Instantiate(box[4], new Vector3(position_x, position_y, 0), Quaternion.identity);
+                    break;
+            }
+            position_x += 0.13f;
+        }
+    }
+}
