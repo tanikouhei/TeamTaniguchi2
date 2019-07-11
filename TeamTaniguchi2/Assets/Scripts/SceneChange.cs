0604256f3ad6ac0a860c9f38ca10c2a9e6daf802
kouhei taniguti <nkc.kouhei.taniguti@gmail.com>
2019-07-11 13:36:30 +0900

    素材コミット

---
+++ TeamTaniguchi2/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+// 1.UIシステムを使うときに必要なライブラリ
+using UnityEngine.UI;
+// 2.Scene関係の処理を行うときに必要なライブラリ
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Screen.SetResolution(540, 960, true);
+    }
+
+    // 3.OnRetry関数が実行されたら、sceneを読み込む
+    public void OnRetryTitle()
+    {
+        // 「ButtonScene」を自分の読み込みたいscene名に変える
+        SceneManager.LoadScene("Title");
+    }
+
+    public void OnRetryMenu()
+    {
+        // 「ButtonScene」を自分の読み込みたいscene名に変える
+        SceneManager.LoadScene("Select");
+    }
+
+    public void OnRetryChoiceStage()
+    {
+        // 「ButtonScene」を自分の読み込みたいscene名に変える
+        SceneManager.LoadScene("Rule");
+    }
+
+    public void OnRetryCollection()
+    {
+        // 「ButtonScene」を自分の読み込みたいscene名に変える
+        SceneManager.LoadScene("Stage");
+    }
+
+    public void OnRetrySetumei()
+    {
+        // 「ButtonScene」を自分の読み込みたいscene名に変える
+        SceneManager.LoadScene("Result");
+    }
+
+    public void EndGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+      Application.Quit();
+#endif
+    }
+}
